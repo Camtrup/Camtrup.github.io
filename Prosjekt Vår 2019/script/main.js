734cc3a4ebde7f50420e66fe5183a626d73a3dcc
Casper Amtrup <casperamtrup@Caspers-MacBook-Pro.local>
2019-04-01 12:54:59 +0200

    u

--- Prosjekt Vår 2019/script/main.js
+++ Prosjekt Vår 2019/script/main.js
@@ -1,23 +1,73 @@
 const main = document.querySelector("main");
-const article = document.querySelector("article");
 
 const db = firebase.database();
-const serier = db.ref("serier")
+const serier = db.ref("serier");
 
 
 function visSerier(snapshot){
     let serier = snapshot.val();
+    let key = snapshot.key;
     main.innerHTML +=`
         <div class="serier">
+            <a href="../serier/serie.html?id=${key}">
             <p class="tittel">${serier.Tittel}</p>
             <img src=${serier.Bilde}>
             <p class="imdb">${serier.Rating}</p>
             <p class="sjanger">Sjanger: ${serier.Sjanger1}  ${serier.Sjanger2}</p>
+            </a>
 
         </div>
     `};
 
     function visAlle(){
-      main.innerHTML="";
-      serier.on("child_added", visSerier);
+      main.innerHTML=""
+      serier.on("child_added",visSerier);
+      }
+
+    function sorterRating(){
+      main.innerHTML=""
+      serier
+        .orderByChild("Rating")
+        .on("child_added", visSerier)
     }
+
+    function kunHBO(){
+      main.innerHTML=""
+      serier
+        .orderByChild("Kanal")
+        .equalTo("HBO")
+        .on("child_added", visSerier)
+        }
+
+    function kunAMC(){
+      main.innerHTML=""
+      serier
+        .orderByChild("kanal")
+        .equalTo("AMC")
+        .on("child_added", visSerier)
+        }
+
+    function kunShowtime(){
+      main.innerHTML=""
+      serier
+        .orderByChild("kanal")
+        .equalTo("Showtime")
+        .on("child_added", visSerier)
+        }
+
+    function kunNetflix(){
+      main.innerHTML=""
+      serier
+        .orderByChild("kanal")
+        .equalTo("Netflix")
+        .on("child_added", visSerier)
+        }
+
+    function kunAmazon(){
+      main.innerHTML=""
+      serier
+        .orderByChild("kanal")
+        .equalTo("Amazon")
+        .on("child_added", visSerier)
+        }
+        visAlle()
